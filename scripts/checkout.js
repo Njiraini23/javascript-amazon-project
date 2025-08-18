c56fa198051128944d54186ce880945a0090a829
Njiraini23 <njiraini88@gmail.com>
2025-08-18 22:06:55 +0300

    generating the HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./checkout/ordersSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
