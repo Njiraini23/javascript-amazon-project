b539e01d84ec2dc65d8c12e539b21c00b32c0fa8
Njiraini23 <njiraini88@gmail.com>
2025-08-18 19:15:25 +0300

    Updating the delivery options and making it interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -33,7 +33,7 @@ cart.forEach((cartItem)=>{
 
     const today = dayjs();
     const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
+      deliveryOptions.deliveryDays,
        'days');
     const dateString = deliveryDate.format(
       'dddd, MMMM D'
@@ -101,20 +101,22 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
      html += `
-    <div class="delivery-option">
-                  <input type="radio"
-                  ${isChecked ? 'checked' : '' }
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      ${dateString}
-                    </div>
-                    <div class="delivery-option-price">
-                      ${priceString} - Shipping
-                    </div>
-                  </div>
-                </div>
+    <div class="delivery-option js-delivery-option"
+    data-product-id = "${matchingProduct.id}"
+    data-delivery-option-id = "${deliveryOption.id}">
+      <input type="radio"
+      ${isChecked ? 'checked' : '' }
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+      <div>
+        <div class="delivery-option-date">
+          ${dateString}
+        </div>
+        <div class="delivery-option-price">
+          ${priceString} - Shipping
+        </div>
+      </div>
+    </div>
    `
   });
   return html;
@@ -134,4 +136,12 @@ document.querySelectorAll('.js-delete-link')
         container.remove();
         
     });
+});
+
+document.querySelectorAll('.js-delivery-option')
+.forEach((element)=>{
+  element.addEventListener('click', ()=>{
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId)
+  });
 });
