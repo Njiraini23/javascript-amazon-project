9bdd9a2b712da2ab43b419bef7f64d76986a6dbf
Njiraini23 <njiraini88@gmail.com>
2025-08-23 13:39:34 +0300

    using promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,5 +3,13 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/backend-practice.js';
 // import '../data/cart-class.js';
 
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve( )
+    })
+}).then(()=>{
+
+})
+
 renderOrderSummary();
 renderPaymentSummary();
