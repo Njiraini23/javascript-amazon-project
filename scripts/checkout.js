e5bc268521121ec3399de19bd65957d36ad70b12
Njiraini23 <njiraini88@gmail.com>
2025-08-23 16:21:59 +0300

    Create order using the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,13 +3,6 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/backend-practice.js';
 // import '../data/cart-class.js';
 
-new Promise((resolve)=>{
-    loadProducts(()=>{
-        resolve( )
-    })
-}).then(()=>{
-
-})
 
 renderOrderSummary();
 renderPaymentSummary();
